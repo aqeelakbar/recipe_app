588a650746819da258543d108870bd1903e08bff
Aqeel Akbar <aqeel@locutus.local>
2017-10-23 05:48:05 +0100

    condensed search file into index

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ app.set('view engine', 'pug');
 app.use(EXPRESS.static(PATH.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	let recipes = UTILS.handleJSON(JSON);
-	res.render('index',{title: 'Search Recipes', json: recipes, number: JSON.recipes.length});
+	let recipes;
+	if(req.query.keywords) {
+			let search = req.query.keywords.split(' ');
+			recipes = UTILS.searchRecipes(JSON, search);
+	} else {
+			recipes = UTILS.handleJSON(JSON);
+	}
+	res.render('index',{title: 'Search Recipes', json: recipes, number: JSON.recipes.length, search: req.query.keywords});
 });
 
 app.get('/recipe', function(req, res){
@@ -26,12 +32,6 @@ app.get('/recipe/:recipeId', function(req, res){
 		recipe = UTILS.singleRecipe(JSON, id);
 	}
 	res.render('recipe',{json: recipe});
-});
-
-app.get('/search', function(req, res){
-	let search = req.query.keywords.split(' ');
-	let recipe = UTILS.searchRecipes(JSON, search);
-	res.render('search',{json: recipe, term: req.query.keywords});
 });
 
 app.listen(8080);
