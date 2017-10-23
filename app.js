c0e8e406343e5bdf45be65bbb7b762df16c78e62
Aqeel Akbar <aqeel@locutus.local>
2017-10-23 08:07:38 +0100

    readdressed variables

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const	EXPRESS = require('express'),
 			PATH = require('path'),
-			UTILS = require('./lib/helper.js'),
+			{searchRecipes, handleJSON, queryHandler, singleRecipe} = require('./lib/helper.js'),
 			JSON = require('./lib/recipes.json');
 
 let app = EXPRESS();
+
+
 
 app.set('views', PATH.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -13,9 +15,9 @@ app.get('/', function(req, res){
 	let recipes;
 	if(req.query.keywords) {
 			let search = req.query.keywords.split(' ');
-			recipes = UTILS.searchRecipes(JSON, search);
+			recipes = searchRecipes(JSON, search);
 	} else {
-			recipes = UTILS.handleJSON(JSON);
+			recipes = handleJSON(JSON);
 	}
 	res.render('index',{title: 'Search Recipes', json: recipes, number: JSON.recipes.length, search: req.query.keywords});
 });
@@ -23,8 +25,8 @@ app.get('/', function(req, res){
 app.get('/recipe/:recipeId', function(req, res){
 	let recipe;
 	if(req.params.recipeId){
-		let id = UTILS.queryHandler(req.params.recipeId);
-		recipe = UTILS.singleRecipe(JSON, id);
+		let id = queryHandler(req.params.recipeId);
+		recipe = singleRecipe(JSON, id);
 	}
 	res.render('recipe',{json: recipe});
 });
